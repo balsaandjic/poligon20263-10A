5949372eea2a1775b1566f4f6081f6791df2f4c4
Ucenik <Ucenik@DESKTOP-A6MV7AL>
2026-03-13 18:35:32 +0100

    v 2.0

--- vektor.cs
+++ vektor.cs
@@ -10,5 +10,27 @@ namespace poligon20263_10A
     {
         public tacka pocetak;
         public tacka kraj;
+        public vektor(tacka A, tacka B)
+        {
+            pocetak = A;
+            kraj = B;
+        }
+        public vektor(tacka A)
+        {
+            tacka poc = new tacka();
+            pocetak = poc;
+            kraj = A;
+        }
+        public tacka centriraj()
+        {
+            double x = kraj.x - pocetak.x;
+            double y = kraj.y - pocetak.y;
+            return new tacka(x, y);
+        }
+        public double SP(vektor a, vektor b)
+        {
+            return 0;
+
+        }
     }
 }
